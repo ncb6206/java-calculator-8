2dc6b7c66c11599dc6551f2188ee817f59d3e457
ncb6206 <ncb6206@gmail.com>
2025-10-20 21:14:44 +0900

    feat: 프롬프트 및 결과 출력 형식 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        int result = calculate(input);
+        System.out.println("결과 : " + result);
     }
 }
 
